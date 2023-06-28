bc6a8385d98fd69fd8096fdb4dd471cf53c25251
Ilya Zavgorodin <103287798+onnkek@users.noreply.github.com>
2023-06-28 04:53:54 +0000

    Update settings page

--- src/components/pages/SettingsPage/NotificationsPage/NotificationsPage.js
+++ src/components/pages/SettingsPage/NotificationsPage/NotificationsPage.js
@@ -1,16 +1,191 @@
-import React from 'react';
-import './NotificationsPage.sass'
+import React from "react";
+import Check from "../../../UI/Icons/Check";
+import "./NotificationsPage.sass";
 
 const NotificationsPage = (props) => {
-    return (
-        <>
-            <div className='settings-item'>
-                <div className="settings-header-container">
-                    <h2 className="settings-header">Notifications preferences</h2>
-                </div>
+  return (
+    <>
+      <div className="settings-item">
+        <div className="settings-header-container">
+          <h2 className="settings-header">Notifications preferences</h2>
+          <div className="settings-check">
+            <div
+              className="settings-check-spinner spinner-border"
+              role="status"
+            ></div>
+            <div className="settings-check-icon">
+              <Check />
             </div>
-        </>
-    );
-}
+            <span className="settings-check-title">Saved</span>
+          </div>
+        </div>
+        <div className="noti-wrapper">
+          <div className="noti-item">
+            <h3 className="settings-subheader">Default notifications task</h3>
+            <div className="settings-descr">
+              Here you can set up notifications at different times for different
+              tasks marked with tags.
+            </div>
+            <div className="noti-options">
+              <div class="input-group mt-3">
+                <div class="checkbox-wrapper input-group-text">
+                  <input
+                    class="checkbox form-check-input mt-0"
+                    type="checkbox"
+                    value=""
+                    aria-label="Checkbox for following text input"
+                  />
+                </div>
+                <input
+                  type="text"
+                  class="form-control"
+                  aria-label="Text input with dropdown button"
+                />
+                <button
+                  class="noti-dropdown btn btn-outline-secondary dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  time before
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      minut(es) before
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      hour(s) before
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      day(s) before
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      month(s) before
+                    </a>
+                  </li>
+                </ul>
+                <span class="noti-options-label input-group-text">for</span>
+                <button
+                  class="noti-dropdown btn btn-outline-secondary dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  task
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      All
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Red badge
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Green badge
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Blue badge
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div class="input-group mt-3">
+                <div class="checkbox-wrapper input-group-text">
+                  <input
+                    class="checkbox form-check-input mt-0"
+                    type="checkbox"
+                    checked
+                    value=""
+                    aria-label="Checkbox for following text input"
+                  />
+                </div>
+                <input
+                  type="text"
+                  class="form-control"
+                  aria-label="Text input with dropdown button"
+                />
+                <button
+                  class="noti-dropdown btn btn-outline-secondary dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  time before
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      minut(es) before
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      hour(s) before
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      day(s) before
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      month(s) before
+                    </a>
+                  </li>
+                </ul>
+                <span class="noti-options-label input-group-text">for</span>
+                <button
+                  class="noti-dropdown btn btn-outline-secondary dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  task
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end">
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      All
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Red badge
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Green badge
+                    </a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      Blue badge
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default NotificationsPage;
--- src/components/pages/SettingsPage/ProfilePage/ProfilePage.js
+++ src/components/pages/SettingsPage/ProfilePage/ProfilePage.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import './ProfilePage.sass'
+import React from "react";
+import "./ProfilePage.sass";
 
 const ProfilePage = (props) => {
-    return (
-        <>
-            <div className='settings-item'>
-                <div className="settings-header-container">
-                    <h2 className="settings-header">Profile preferences</h2>
-                </div>
+  return (
+    <>
+      <div className="settings-item">
+        <div className="settings-header-container">
+          <h2 className="settings-header">Profile preferences</h2>
+        </div>
+        <form className="form-1 col-8">
+          <div className="profile-form-group">
+            <label className="profile-form-group-label form-label">
+              Username
+            </label>
+            <input
+              className="profile-form-group-input form-control"
+              value="username"
+            />
+            <div className="profile-from-group-descr">
+              Your username in the glider system and in the header of the site
+              to display authorization information.
             </div>
-        </>
-    );
-}
+          </div>
+          <div className="profile-form-group">
+            <label className="profile-form-group-label form-label">Email</label>
+            <input
+              className="profile-form-group-input form-control"
+              value="email@email.com"
+            />
+            <div className="profile-from-group-descr">
+              Your e-mail in the planner's system for the possibility of account
+              recovery in case of password loss. Your email is not displayed
+              publicly for privacy reasons.
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default ProfilePage;
